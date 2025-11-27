b2afc4acd8e41954b4359de05d7d9fae3779d194
Sid-si <andertwcc@gmail.com>
2025-11-27 15:35:24 -0300

    correções 2

--- src/componentes/CampoDeEntrada/index.jsx
+++ src/componentes/CampoDeEntrada/index.jsx
@@ -1,4 +1,4 @@
-import '.Campo-de-entrada.estilos.css';
+import '.campo-de-entrada.estilos.css';
 export function CampoDeEntrada(props) {
   return <input {...props}  className='campo-entrada'/>;
 }
